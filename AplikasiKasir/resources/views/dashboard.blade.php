6e9f36719290fc5c7bfd027f55899ea75c9ce1a9
unknown <revaldydwi22@gmail.com>
2025-02-17 09:02:13 +0700

    merge

--- AplikasiKasir/resources/views/dashboard.blade.php
+++ AplikasiKasir/resources/views/dashboard.blade.php
@@ -22,28 +22,7 @@
     </ul>
 </div>
 
-<<<<<<< HEAD
-    <div>
-        <h2>{{ Auth::user()->name }}</h2> 
-    </div>
-
-    <div>
-        <ul class="nav-links">
-            <li><a href="#dashboard">Dashboard</a></li>
-            <li><a href="#profile">Absen Petugas</a></li>
-            <li><a href="#settings">Settings</a></li>
-            <li>
-                <form action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button type="submit">LogOut</button>
-                </form>
-            </li>
-        </ul>
-    </div>
-
-=======
 <div class="main-content">
->>>>>>> 91849f4175e2bfe4077db016380363b3386f46f1
     <div id="dashboard">
         <div class="dashboard-menu">
             <a href="#" onclick="showContent('produk')">Produk</a>
@@ -66,9 +45,6 @@
                 <tr>
                     <td>{{ $product->nama_produk }}</td>
                     <td>{{ $product->stok }}</td>
-<<<<<<< HEAD
-                    <td>Rp{{ number_format($product->harga, 0, '', '.')}}</td>
-=======
                     <td>Rp{{ number_format($product->harga, 0, '', '.') }}</td>
                 </tr>
                 @endforeach
@@ -106,15 +82,11 @@
                     <td>{{ $penjualan->tanggal_penjualan }}</td>
                     <td>Rp{{ number_format($penjualan->total_harga, 0, '', '.') }}</td>
                     <td>{{ $penjualan->pelanggan_id }}</td>
->>>>>>> 91849f4175e2bfe4077db016380363b3386f46f1
                 </tr>
                 @endforeach
             </table>
         </div>
-<<<<<<< HEAD
-=======
     </div>
->>>>>>> 91849f4175e2bfe4077db016380363b3386f46f1
 </div>
 </body>
 </html>
