9af54f429b0af6023efd62f698c27b7820696834
unknown <revaldydwi22@gmail.com>
2025-02-23 20:19:49 +0700

    tampilan petugas(petugas tidak bisa tambah dan hapus produk)

--- AplikasiKasir/resources/views/produk/index.blade.php
+++ AplikasiKasir/resources/views/produk/index.blade.php
@@ -4,7 +4,10 @@
 <div class="container">
     <h3 class="mb-3 text-center">Daftar Produk</h3>
 
+    @if(Auth::user()->role === 'admin')
     <a href="{{ route('produk.create') }}" class="btn btn-primary mb-3">Tambah Produk</a>
+    @endif
+
 
     @if(session('message'))
         <div class="alert alert-success">{{ session('message') }}</div>
@@ -29,11 +32,13 @@
                     <td>{{ $product->stok }}</td>
                     <td class="border-dark">
                         <a href="{{ route('produk.edit', $product->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                        @if(Auth::user()->role === 'admin')
                         <form action="{{ route('produk.destroy', $product->id) }}" method="POST" class="d-inline">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Yakin hapus?')">Hapus</button>
                         </form>
+                        @endif
                     </td>
                 </tr>
                 @endforeach
